1531f035d150491384e61deafec0379849150bcf
Collin Boots <cboots@seas.upenn.edu>
2013-11-22 16:10:22 -0500

    Added some basic event handling schemes

--- RGBD-to-Mesh/RGBDDevice.cpp
+++ RGBD-to-Mesh/RGBDDevice.cpp
@@ -1,3 +1,85 @@
 #include "RGBDDevice.h"
 
+void RGBDDevice::addNewRGBDFrameListener(NewRGBDFrameListener* listener)
+{
+	mNewRGBDFrameListeners.push_back(listener);
+}
 
+void RGBDDevice::addDeviceConnectedListener(DeviceConnectedListener* listener)
+{
+	mDeviceConnectedListeners.push_back(listener);
+}
+
+void RGBDDevice::addDeviceDisconnectedListener(DeviceDisconnectedListener* listener)
+{
+	mDeviceDisconnectedListeners.push_back(listener);
+}
+
+void RGBDDevice::addDeviceMessageListener(DeviceMessageListener* listener)
+{
+	mDeviceMessageListeners.push_back(listener);
+}
+
+void RGBDDevice::removeNewRGBDFrameListener(NewRGBDFrameListener* listener)
+{
+	std::vector<NewRGBDFrameListener*>::iterator position = std::find(mNewRGBDFrameListeners.begin(), mNewRGBDFrameListeners.end(), listener);
+
+	if (position != mNewRGBDFrameListeners.end()) // == vector.end() means the element was not found
+		mNewRGBDFrameListeners.erase(position);
+
+}
+
+void RGBDDevice::removeDeviceConnectedListener(DeviceConnectedListener* listener)
+{
+	std::vector<DeviceConnectedListener*>::iterator position = std::find(mDeviceConnectedListeners.begin(), mDeviceConnectedListeners.end(), listener);
+
+	if (position != mDeviceConnectedListeners.end()) // == vector.end() means the element was not found
+		mDeviceConnectedListeners.erase(position);
+
+}
+
+void RGBDDevice::removeDeviceDisconnectedListener(DeviceDisconnectedListener* listener)
+{
+	std::vector<DeviceDisconnectedListener*>::iterator position = std::find(mDeviceDisconnectedListeners.begin(), mDeviceDisconnectedListeners.end(), listener);
+
+	if (position != mDeviceDisconnectedListeners.end()) // == vector.end() means the element was not found
+		mDeviceDisconnectedListeners.erase(position);
+
+}
+
+void RGBDDevice::removeDeviceMessageListener(DeviceMessageListener* listener)
+{
+	std::vector<DeviceMessageListener*>::iterator position = std::find(mDeviceMessageListeners.begin(), mDeviceMessageListeners.end(), listener);
+
+	if (position != mDeviceMessageListeners.end()) // == vector.end() means the element was not found
+		mDeviceMessageListeners.erase(position);
+
+}
+
+void RGBDDevice::onNewRGBDFrame(RGBDFramePtr frame)
+{
+	for(std::vector<NewRGBDFrameListener*>::iterator it = mNewRGBDFrameListeners.begin(); it != mNewRGBDFrameListeners.end(); ++it) {
+		(*it)->onNewRGBDFrame(frame);
+	}
+}
+
+void RGBDDevice::onConnect()
+{
+	for(std::vector<DeviceConnectedListener*>::iterator it = mDeviceConnectedListeners.begin(); it != mDeviceConnectedListeners.end(); ++it) {
+		(*it)->onDeviceConnected();
+	}
+}
+
+void RGBDDevice::onDisconnect()
+{
+	for(std::vector<DeviceDisconnectedListener*>::iterator it = mDeviceDisconnectedListeners.begin(); it != mDeviceDisconnectedListeners.end(); ++it) {
+		(*it)->onDeviceDisconnected();
+	}
+}
+
+void RGBDDevice::onMessage(std::string msg)
+{
+	for(std::vector<DeviceMessageListener*>::iterator it = mDeviceMessageListeners.begin(); it != mDeviceMessageListeners.end(); ++it) {
+		(*it)->onMessage(msg);
+	}
+}
--- RGBD-to-Mesh/RGBDDevice.h
+++ RGBD-to-Mesh/RGBDDevice.h
@@ -1,13 +1,75 @@
 #pragma once
-class RGBDDevice
+#include "RGBDFrame.h"
+#include <string>
+#include <vector>
+#include <algorithm>
+
+enum ImageRegistrationMode
 {
-private:
-	RGBDDevice(void);
-public:
-	virtual ~RGBDDevice(void) = 0;
+	REGISTRATION_OFF = 0,
+	REGISTRATION_DEPTH_TO_COLOR = 1
 };
 
+class RGBDDevice
+{
+	//******************Events and listener declarations*****************
+public:
+	class NewRGBDFrameListener{
+	public :
+		virtual void onNewRGBDFrame(RGBDFramePtr frame) = 0;
+	};
 
-//Even virtual destructors MUST exist
-RGBDDevice::~RGBDDevice(void){ }
+	class DeviceConnectedListener{
+	public :
+		virtual void onDeviceConnected() = 0;
+	};
 
+	class DeviceDisconnectedListener{
+	public :
+		virtual void onDeviceDisconnected() = 0;
+	};
+
+	class DeviceMessageListener{
+	public :
+		virtual void onMessage(std::string msg) = 0;
+	};
+
+protected:
+	std::vector<NewRGBDFrameListener*> mNewRGBDFrameListeners;
+	std::vector<DeviceConnectedListener*> mDeviceConnectedListeners;
+	std::vector<DeviceDisconnectedListener*> mDeviceDisconnectedListeners;
+	std::vector<DeviceMessageListener*> mDeviceMessageListeners;
+
+public:
+	virtual ~RGBDDevice(void){};
+
+	virtual void start() = 0;
+
+	//Override to enable streams
+	virtual bool hasDepthStream() {return false;}
+	virtual bool hasColorStream() {return false;}
+
+	//Return true if successful (false usually means this device doesn't support the functionality)
+	virtual bool createColorStream() { return false;}
+	//Return true if successful (false usually means this device doesn't support the functionality)
+	virtual bool createDepthStream() { return false;}
+	//Return true if successful (false usually means this device doesn't support the functionality)
+	virtual bool setImageRegistrationMode(ImageRegistrationMode mode) {return false;}
+
+	virtual ImageRegistrationMode getImageRegistrationMode(ImageRegistrationMode mode) {return REGISTRATION_OFF;}
+
+	void addNewRGBDFrameListener(NewRGBDFrameListener* listener);
+	void addDeviceConnectedListener(DeviceConnectedListener* listener);
+	void addDeviceDisconnectedListener(DeviceDisconnectedListener* listener);
+	void addDeviceMessageListener(DeviceMessageListener* listener);
+
+	void removeNewRGBDFrameListener(NewRGBDFrameListener* listener);
+	void removeDeviceConnectedListener(DeviceConnectedListener* listener);
+	void removeDeviceDisconnectedListener(DeviceDisconnectedListener* listener);
+	void removeDeviceMessageListener(DeviceMessageListener* listener);
+
+	void onNewRGBDFrame(RGBDFramePtr frame);
+	void onConnect();
+	void onDisconnect();
+	void onMessage(std::string msg);
+};
