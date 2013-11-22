ba97f5396ce1752cf688b9ebcabe5bad13687ba8
Collin Boots <cboots@seas.upenn.edu>
2013-11-22 16:36:27 -0500

    Added more virtual methods.

--- RGBD-to-Mesh/ONIKinectDevice.h
+++ RGBD-to-Mesh/ONIKinectDevice.h
@@ -6,5 +6,6 @@ class ONIKinectDevice :
 public:
 	ONIKinectDevice(void);
 	~ONIKinectDevice(void);
+
 };
 
--- RGBD-to-Mesh/RGBDDevice.h
+++ RGBD-to-Mesh/RGBDDevice.h
@@ -10,6 +10,18 @@ enum ImageRegistrationMode
 	REGISTRATION_DEPTH_TO_COLOR = 1
 };
 
+typedef enum
+{
+	DEVICESTATUS_OK = 0,
+	DEVICESTATUS_ERROR = 1,
+	DEVICESTATUS_NOT_IMPLEMENTED = 2,
+	DEVICESTATUS_NOT_SUPPORTED = 3,
+	DEVICESTATUS_BAD_PARAMETER = 4,
+	DEVICESTATUS_OUT_OF_FLOW = 5,
+	DEVICESTATUS_NO_DEVICE = 6,
+	DEVICESTATUS_TIME_OUT = 102,
+} DeviceStatus;
+
 class RGBDDevice
 {
 	//******************Events and listener declarations*****************
@@ -43,20 +55,25 @@ protected:
 public:
 	virtual ~RGBDDevice(void){};
 
-	virtual void start() = 0;
+	//Purely virtual methods
+	virtual DeviceStatus initialize(void) = 0;
+	virtual DeviceStatus connect(void) = 0;
+	virtual DeviceStatus disconnect(void) = 0;
+
 
 	//Override to enable streams
-	virtual bool hasDepthStream() {return false;}
-	virtual bool hasColorStream() {return false;}
+	inline virtual bool hasDepthStream() {return false;}
+	inline virtual bool hasColorStream() {return false;}
 
 	//Return true if successful (false usually means this device doesn't support the functionality)
-	virtual bool createColorStream() { return false;}
+	inline virtual bool createColorStream() { return false;}
 	//Return true if successful (false usually means this device doesn't support the functionality)
-	virtual bool createDepthStream() { return false;}
-	//Return true if successful (false usually means this device doesn't support the functionality)
-	virtual bool setImageRegistrationMode(ImageRegistrationMode mode) {return false;}
+	inline virtual bool createDepthStream() { return false;}
 
-	virtual ImageRegistrationMode getImageRegistrationMode(ImageRegistrationMode mode) {return REGISTRATION_OFF;}
+	//Return true if successful (false usually means this device doesn't support the functionality)
+	inline virtual bool setImageRegistrationMode(ImageRegistrationMode) {return false;}
+
+	inline virtual ImageRegistrationMode getImageRegistrationMode(void) {return REGISTRATION_OFF;}
 
 	void addNewRGBDFrameListener(NewRGBDFrameListener* listener);
 	void addDeviceConnectedListener(DeviceConnectedListener* listener);
