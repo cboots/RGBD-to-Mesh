a1f3c439401ab8d375da21410302a819c93b3441
Collin Boots <cboots@seas.upenn.edu>
2013-11-22 01:22:14 -0500

    Bunch of self explanatory getters/setters.

--- RGBD-to-Mesh/RGBDFrame.h
+++ RGBD-to-Mesh/RGBDFrame.h
@@ -23,28 +23,95 @@ typedef struct
 	uint16_t depth;
 } DepthPixel;
 
+typedef uint64_t timestamp;
+
 
 //Forward declaration
 class RGBDFrame;
 typedef shared_ptr<RGBDFrame> frame_ptr;
+typedef shared_ptr<DepthPixel[]> depth_array_ptr;
+typedef shared_ptr<ColorPixel[]> color_array_ptr;
 
 class RGBDFrame
 {
-
 protected:
-	DepthPixel* depthData;
-	ColorPixel* colorData;
+	int mXRes, mYRes;
+	timestamp mDepthTime, mColorTime;
+	bool mHasDepth, mHasColor;
+
+	depth_array_ptr mDepthData;
+	color_array_ptr mColorData;
 
 public:
 	RGBDFrame(void);
 	~RGBDFrame(void);
 
-	bool hasDepth();
-	bool hasColor();
-	int getXRes();
-	int getYRes();
 
-	void setDepthData
+	void setResolution(int width, int height);
+	void clearDepthImage();
+	void clearColorImage();
+	
 
+	inline color_array_ptr getColorArray()
+	{
+		return mColorData;
+	}
+
+	inline depth_array_ptr getDepthArray()
+	{
+		return mDepthData;
+	}
+
+	
+	inline void setHasDepth(bool hasDepth)
+	{
+		mHasDepth = hasDepth;
+	}
+
+	inline void setHasColor(bool hasColor)
+	{
+		mHasDepth = hasColor;
+	}
+
+
+	inline bool hasDepth()
+	{
+		return mHasDepth;
+	}
+
+	inline bool hasColor()
+	{
+		return mHasColor;
+	}
+	
+	inline int getXRes()
+	{
+		return mXRes;
+	}
+
+	inline int getYRes()
+	{
+		return mYRes;
+	}
+
+	inline void setDepthTimestamp(timestamp depthTime)
+	{
+		mDepthTime = depthTime;
+	}
+
+	inline void setColorTimestamp(timestamp colorTime)
+	{
+		mColorTime = colorTime;
+	}
+
+	inline timestamp getDepthTimestamp()
+	{
+		return mDepthTime;
+	}
+
+	inline timestamp getColorTimestamp()
+	{
+		return mColorTime;
+	}
 };
 
