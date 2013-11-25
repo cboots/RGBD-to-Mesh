7abfec9943cf4737ae6bafb9f80931048d945aa3
Collin Boots <cboots@seas.upenn.edu>
2013-11-25 16:06:22 -0500

    Ignore recording output.

--- RGBD-to-Mesh/main.cpp
+++ RGBD-to-Mesh/main.cpp
@@ -53,10 +53,10 @@ int main(int argc, char** argv)
 	}
 
 	
-	//LogDevice device;
-	//device.setSourceDirectory("logs\\recording");
-	//device.setLoopStreams(true);
-	ONIKinectDevice device;
+	LogDevice device;
+	device.setSourceDirectory("logs\\recording");
+	device.setLoopStreams(true);
+	//ONIKinectDevice device;
 	RGBDDeviceListener deviceStateListener;
 	RGBDFrameListener frameListener;
 	device.addDeviceConnectedListener(&deviceStateListener);
