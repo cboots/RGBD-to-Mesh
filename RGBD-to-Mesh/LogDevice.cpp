b6d9cbe3f11ef12d9cf1fc70e6609fb48db71468
Collin Boots <cboots@seas.upenn.edu>
2013-11-25 00:22:17 -0500

    Started to implement log device

--- RGBD-to-Mesh/LogDevice.cpp
+++ RGBD-to-Mesh/LogDevice.cpp
@@ -9,3 +9,115 @@ LogDevice::LogDevice(void)
 LogDevice::~LogDevice(void)
 {
 }
+
+
+
+DeviceStatus LogDevice::initialize(void)
+{
+	return DEVICESTATUS_OK;	
+}
+
+DeviceStatus LogDevice::connect(void)
+{
+
+	std::ostringstream logfilePath; 
+	logfilePath << mDirectory << "\\log.xml" << endl;
+	if(fileExists(logfilePath.str()))
+	{
+		onConnect();
+
+		//Load log.
+
+		return DEVICESTATUS_OK;
+	}else{
+		std::ostringstream out; 
+		out << "Couldn't open log file" << endl;
+		onMessage(out.str());
+
+		return DEVICESTATUS_NO_DEVICE;
+	}
+
+}
+
+DeviceStatus LogDevice::disconnect(void)
+{
+	onDisconnect();
+	return DEVICESTATUS_OK;
+}
+
+
+
+DeviceStatus LogDevice::shutdown(void)
+{
+	return DEVICESTATUS_OK;
+}
+
+
+bool LogDevice::hasDepthStream()
+{
+	//TODO: implement
+	return false;
+}
+
+bool LogDevice::hasColorStream()
+{
+	//TODO: implement
+	return false;
+}
+
+bool LogDevice::createColorStream() 
+{
+	//TODO: Implement
+	return false;
+}
+
+bool LogDevice::createDepthStream() 
+{
+	//TODO: Implement
+	return false;
+}
+
+bool LogDevice::destroyColorStream()  
+{
+	return false;
+}
+
+bool LogDevice::destroyDepthStream()
+{
+	return false;
+}
+
+RGBDImageRegistrationMode LogDevice::getImageRegistrationMode(void)
+{
+	return RGBDImageRegistrationMode::REGISTRATION_OFF;
+}
+
+int LogDevice::getDepthResolutionX()
+{
+	return mXRes;
+}
+int LogDevice::getDepthResolutionY()
+{
+	return mYRes;
+}
+
+int LogDevice::getColorResolutionX()
+{
+	return mXRes;
+}
+
+int LogDevice::getColorResolutionY()
+{
+	return mYRes;
+}
+
+
+bool LogDevice::isDepthStreamValid() 
+{
+	return false;
+}
+
+bool LogDevice::isColorStreamValid()
+{
+	return false;
+}
--- RGBD-to-Mesh/LogDevice.h
+++ RGBD-to-Mesh/LogDevice.h
@@ -1,10 +1,52 @@
 #pragma once
 #include "rgbddevice.h"
+#include "RGBDFrameFactory.h"
+#include <string>
+#include "FileUtils.h"
+#include <ostream>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
 class LogDevice :
 	public RGBDDevice
 {
+protected:
+	RGBDFrameFactory mFrameFactory;
+	string mDirectory;
+
+	int mXRes,mYRes;
 public:
 	LogDevice(void);
 	~LogDevice(void);
+	
+	DeviceStatus initialize(void)  override;//Initialize 
+	DeviceStatus connect(void)	   override;//Connect to any device
+	DeviceStatus disconnect(void)  override;//Disconnect from current device
+	DeviceStatus shutdown(void) override;
+
+	void setSourceDirectory(string dir) { mDirectory = dir;}
+	string setSourceDirectory(){return mDirectory;}
+
+	bool hasDepthStream() override;
+	bool hasColorStream() override;
+
+	bool createColorStream() override;
+	bool createDepthStream() override;
+
+	bool destroyColorStream()  override;
+	bool destroyDepthStream()  override;
+	
+	bool setImageRegistrationMode(RGBDImageRegistrationMode) override;
+	RGBDImageRegistrationMode getImageRegistrationMode(void) override;
+
+	int getDepthResolutionX() override;
+	int getDepthResolutionY() override;
+	int getColorResolutionX() override;
+	int getColorResolutionY() override;
+	bool isDepthStreamValid() override;
+	bool isColorStreamValid() override;
+
 };
 
