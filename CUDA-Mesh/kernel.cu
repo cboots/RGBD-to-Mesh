ce144149415b17a92df9aaf514792d95454a693e
daltonb <dbanks@seas.upenn.edu>
2013-12-05 16:43:16 -0500

    added point cloud kernel

--- CUDA-Mesh/kernel.cu
+++ CUDA-Mesh/kernel.cu
@@ -1,16 +1,44 @@
 
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
+#include <stdint.h>
 
 #include <stdio.h>
+#include "device_structs.h"
 
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
+/*
+#define FOV_Y 43 # degrees
+#define FOV_X 57
 
+#define SCALE_Y tan((FOV_Y/2)*pi/180)
+#define SCALE_X tan((FOV_X/2)*pi/180)
+*/
+#define SCALE_Y 0.393910475614942392
+#define SCALE_X 0.542955699638436879
+
+__global__ void makePointCloud(ColorPixel* colorPixels, DPixel* dPixels, int xRes, int yRes, PointCloud* pointCloud) {
+    int i = blockIdx.x*(blockDim.x*blockDim.y) + (threadIdx.x*blockDim.y) + threadIdx.y;
+    int r = i / xRes;
+    int c = i % xRes;
+
+    if (dPixels[i].depth > 0.0f) {
+        float u = (c - (xRes-1)/2.0f + 1) / (xRes-1); // image plane u coordinate
+        float v = ((yRes-1)/2.0f - r) / (yRes-1); // image plane v coordinate
+        float Z = dPixels[i].depth/1000.0f; // depth in mm
+        pointCloud[i].pos = glm::vec3(u*Z*SCALE_X, v*Z*SCALE_Y, Z); // convert uv to XYZ
+        pointCloud[i].color = glm::vec3(colorPixels[i].r, colorPixels[i].g, colorPixels[i].b); // copy over texture
+    }
+}
+
+//cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
+
+/*
 __global__ void addKernel(int *c, const int *a, const int *b)
 {
     int i = threadIdx.x;
     c[i] = a[i] + b[i];
 }
+*/
 /*
 int main()
 {
@@ -41,6 +69,7 @@ int main()
 }
 */
 
+/*
 // Helper function for using CUDA to add vectors in parallel.
 cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
 {
@@ -120,3 +149,4 @@ Error:
     
     return cudaStatus;
 }
+*/
