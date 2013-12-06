7e904798a071b956b080d9cc8c566e1772fc426e
daltonb <dbanks@seas.upenn.edu>
2013-12-05 21:07:09 -0500

    started on eigenvector calculation

--- CUDA-Mesh/device_structs.h
+++ CUDA-Mesh/device_structs.h
@@ -1,14 +1,22 @@
 #pragma once
 #include <stdint.h>
 #include <cuda.h>
+#define GLM_FORCE_RADIANS
 #include "glm/glm.hpp"
+#include "glm/gtc/constants.hpp"
 
 typedef struct
 {
-	/* Point location */
+	// Point location
 	glm::vec3 pos;
-	/* RGB point color */
+	// RGB point color
 	glm::vec3 color;
-	/* Normalized point normal vector */
+	// Normalized point normal vector
 	glm::vec3 normal;
 } PointCloud;
+
+typedef struct
+{
+    glm::vec3 eigenVals;
+    glm::mat3 eigenVecs;
+} EigenResult;
--- CUDA-Mesh/kernel.cu
+++ CUDA-Mesh/kernel.cu
@@ -5,6 +5,7 @@
 
 #include <stdio.h>
 #include "device_structs.h"
+#include "RGBDFrame.h"
 
 /*
 #define FOV_Y 43 # degrees
@@ -15,6 +16,7 @@
 */
 #define SCALE_Y 0.393910475614942392
 #define SCALE_X 0.542955699638436879
+#define PI      3.141592653589793238
 
 __global__ void makePointCloud(ColorPixel* colorPixels, DPixel* dPixels, int xRes, int yRes, PointCloud* pointCloud) {
     int i = (blockIdx.y*gridDim.x + blockIdx.x)*(blockDim.y*blockDim.x) + (threadIdx.y*blockDim.x) + threadIdx.x;
@@ -28,4 +30,43 @@ __global__ void makePointCloud(ColorPixel* colorPixels, DPixel* dPixels, int xRe
         pointCloud[i].pos = glm::vec3(u*Z*SCALE_X, v*Z*SCALE_Y, Z); // convert uv to XYZ
         pointCloud[i].color = glm::vec3(colorPixels[i].r, colorPixels[i].g, colorPixels[i].b); // copy over texture
     }
+}
+
+__device__ EigenResult eigenSymmetric33(glm::mat3 A) {
+    // Given a real symmetric 3x3 matrix A, computes the eigenvalues and eigenvectors
+    EigenResult result;
+    // Compute eigenvalues
+    // see: http://en.wikipedia.org/wiki/Eigenvalue_algorithm#3.C3.973_matrices
+    float p1 = pow(A[0][1], 2) + pow(A[0][2], 2) + pow(A[1][2], 2);
+    if (p1 == 0) { // A is diagonal
+        result.eigenVals = glm::vec3(A[0][0], A[1][1], A[2][2]);
+    } else {
+        float q = (A[0][0] + A[1][1] + A[2][2])/3.0f; // mean(trace(A))
+        float p2 = pow(A[0][0]-q, 2) + pow(A[1][1]-q, 2) + pow(A[2][2]-q, 2) + 2*p1;
+        float p = sqrt(p2/6);
+        glm::mat3 B = (1/p) * (A-q*glm::mat3(1.0f));
+        float r = glm::determinant(B)/2;
+        // theoretically -1 <= r <= 1, but clamp in case of numeric error
+        float phi;
+        if (r <= -1) {
+            phi = PI / 3;
+        } else if (r >= 1) {
+            phi = 0;
+        } else {
+            phi = glm::acos(r)/3;
+        }
+        result.eigenVals.x = q + 2*p*glm::cos(phi);
+        result.eigenVals.z = q + 2*p*glm::cos(phi + 2*PI/3);
+        result.eigenVals.y = 3*q - result.eigenVals.x - result.eigenVals.z;
+    }
+    // Compute eigenvectors
+    //glm::vec3 eigVec1 = glm::cross(A[0] - glm::vec3(0.0f, result.eigenVals[0], 0.0f
+    // TODO: refactor to impose condition on smallest eigenvalue and just return normal vector
+}
+
+__global__ void computePointNormals(PointCloud* pointCloud, int xRes, int yRes) {
+    int i = (blockIdx.y*gridDim.x + blockIdx.x)*(blockDim.y*blockDim.x) + (threadIdx.y*blockDim.x) + threadIdx.x;
+    int r = i / xRes;
+    int c = i % xRes;
+
 }
