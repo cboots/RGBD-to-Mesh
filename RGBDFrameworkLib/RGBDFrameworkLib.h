0ad2081bcbe41f5ab9a6bfdad5508ee8f26be291
Collin Boots <cboots@seas.upenn.edu>
2013-12-29 16:17:18 -0600

    Added master include file

---
+++ RGBDFrameworkLib/RGBDFrameworkLib.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "FileUtils.h"
+#include "FrameLogger.h"
+#include "LogDevice.h"
+#include "ONIKinectDevice.h"
+#include "RGBDFrame.h"
+#include "RGBDDevice.h"
+#include "RGBDFrameFactory.h"
+
