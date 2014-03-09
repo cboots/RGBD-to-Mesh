280409284cc1553418d4419d4db9cf12c7dca3c5
Collin Boots <cboots@seas.upenn.edu>
2014-03-09 11:49:29 -0400

    Generated simple test dataset for EM algorithm

--- docs/algorithmdev/em1d.m
+++ docs/algorithmdev/em1d.m
@@ -1,1 +1,28 @@
+ clear all
+ close all
  
+ 
+ bins = zeros(1, 512);
+ 
+ trueclustermeans =     [100 256 300 375 450];
+ trueclustersigmas=     [1000 2 1000 200 100];
+ trueclusterpeakcounts= [1000 200 900 600 500];
+ noise=0.1;
+ 
+ 
+ ind = 1:512;
+ for i=1:length(trueclustermeans)
+    diff = ind-trueclustermeans(i);
+    expont = -diff.*diff/(2*trueclustersigmas(i));
+    bins = bins + trueclusterpeakcounts(i)*exp(expont);
+ end
+ 
+ figure
+ plot(bins);
+ axis([1 512 0 5000])
+ 
+ bins = bins.*(1 + noise*rand(1, 512));
+ 
+ figure
+ plot(bins);
+ axis([1 512 0 5000])
