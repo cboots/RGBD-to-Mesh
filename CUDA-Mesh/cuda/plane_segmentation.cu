42a58a79044a85eaeb5eb0dd28ab1ee8be061703
Collin Boots <cboots@seas.upenn.edu>
2014-03-11 10:16:16 -0400

    Added simple histogram peak detection algorithm based on simple gaussian model

--- CUDA-Mesh/cuda/plane_segmentation.cu
+++ CUDA-Mesh/cuda/plane_segmentation.cu
@@ -96,3 +96,114 @@ __host__ void ACosHistogram(float* cosineValue, int* histogram, int valueCount, 
 }
 
 #pragma endregion
+
+
+#pragma region Simple Histogram Peak Detection
+
+__global__ void gaussianSubtractionPeakDetectionKernel(int* histx, int* histy, int* histz, int* peaksX, int* peaksY, int* peaksZ, 
+													   int histLength, int maxPeaks, int minPeakCount, glm::vec3 sigma2inv)
+{
+	//Setup shared buffers
+	extern __shared__ int s_temp[];
+	int* s_hist = s_temp;
+	int* s_max = s_hist + histLength;
+	int* s_maxI = s_hist + histLength/2;
+	int* s_peaks = s_max + histLength/2;
+
+	//Load histogram from different location for each block
+	float sig = sigma2inv[blockIdx.x];
+	if(blockIdx.x == 0)
+		s_hist[threadIdx.x] = histx[threadIdx.x];
+	else if(blockIdx.x == 1)
+		s_hist[threadIdx.x] = histy[threadIdx.x];
+	else //if(blockIdx.x == 2)
+		s_hist[threadIdx.x] = histz[threadIdx.x];
+
+	//clear peaks
+	if(threadIdx.x < maxPeaks)
+		s_peaks[threadIdx.x] = -1;
+
+	__syncthreads();
+	//====Load/Init Complete=====
+	//====Begin Peak Loop =======
+
+	//For up to the maximum number of peaks
+	for(int peaki = 0; peaki < maxPeaks; ++peaki)
+	{
+
+#pragma region Maximum Finder
+		//========Compute maximum=======
+		//First step loads from main hist, so do outside loop
+		int halfpoint = histLength >> 1;
+		int thread2 = threadIdx.x + halfpoint;
+
+		if(threadIdx.x < halfpoint)
+		{
+			int temp = s_hist[thread2];
+			bool leftSmaller = (s_hist[threadIdx.x] < temp);
+			s_max[threadIdx.x] = leftSmaller?temp:s_hist[threadIdx.x];
+			s_maxI[threadIdx.x] = leftSmaller?thread2:threadIdx.x;
+		}
+		__syncthreads();
+		while(halfpoint > 0)
+		{
+			halfpoint >>= 1;
+			if(threadIdx.x < halfpoint)
+			{
+				thread2 = threadIdx.x + halfpoint;
+				int temp = s_max[thread2];
+				if (temp > s_max[threadIdx.x]) {
+					s_max[threadIdx.x] = temp;
+					s_maxI[threadIdx.x] = s_maxI[thread2];
+				}
+			}
+			__syncthreads();
+		}
+
+		//========Compute maximum End=======
+#pragma endregion
+
+		if(threadIdx.x == 0)
+		{
+			s_peaks[peaki] = s_maxI[0];
+		}
+
+		if(s_max[0] < minPeakCount)
+			break;//done. No more peaks to find
+
+		//=====Subtract gaussian model=====
+		int diff = (threadIdx.x-s_maxI[peaki]);
+		s_hist[threadIdx.x] -= s_max[0] * expf(-diff*diff*sig);
+
+		__syncthreads();
+	}
+
+	//Writeback
+	if(threadIdx.x < maxPeaks)
+	{
+		if(blockIdx.x == 0)
+			peaksX[threadIdx.x] = s_peaks[threadIdx.x];
+		else if(blockIdx.x == 1)
+			peaksY[threadIdx.x] = s_peaks[threadIdx.x];
+		else //if(blockIdx.x == 2)
+			peaksZ[threadIdx.x] = s_peaks[threadIdx.x];
+	}
+}
+
+__host__ void gaussianSubtractionPeakDetection(Int3SOA decoupledHist, Int3SOA peakIndex, int histSize, int maxPeaks, int minPeakCount, glm::vec3 sigmas)
+{
+	assert(histSize > 32);
+	assert(!(histSize & (histSize - 1))); //Assert is power of two
+	assert(histSize % 32 == 0);//Assert is multiple of 32
+
+	int sharedSize = (histSize*2 + maxPeaks)*sizeof(int);
+	dim3 threads(histSize);
+	dim3 blocks(3);
+
+	gaussianSubtractionPeakDetectionKernel<<<blocks,threads,sharedSize>>>(decoupledHist.x, decoupledHist.y, decoupledHist.z, 
+		peakIndex.x, peakIndex.y, peakIndex.z, histSize, maxPeaks, minPeakCount, 1.0f/(2.0f*sigmas*sigmas));
+
+}
+
+
+#pragma endregion
---
+++ docs/samplehistogramdata/histogramanalysis.m
@@ -0,0 +1,96 @@
+close all
+clear all
+
+%% Import the data
+%data = xlsread('decoupledhistogrammixedscene.csv');
+%data = xlsread('decoupledhistogramflatwallheadon.csv');
+data = xlsread('decoupledhistogramuppercorner.csv');
+%data = xlsread('decoupledhistogramceiling.csv');
+%data = xlsread('decoupledhistogramnoplanes.csv');
+
+%% Allocate imported array to column variable names
+normXHist = data(:,1);
+normYHist = data(:,2);
+normZHist = data(:,3);
+
+%% Clear temporary variables
+clearvars data raw columnIndices;
+
+bins = [1:512]';
+
+figure
+plot(bins, normXHist, bins, normYHist, bins, normZHist);
+title('histogram');
+
+testXHist = normXHist;
+testYHist = normYHist;
+testZHist = normZHist;
+
+range = 64;
+%%X histogram
+[peakV peakI] = max(testXHist);
+sigma = 15;
+peaksX = [];
+
+figure
+plot(bins,testXHist);
+hold all
+while peakV > 500
+    peaksX = [peaksX peakI];
+    gauss = peakV * exp( - (bins-peakI).*(bins-peakI) / (2*sigma*sigma));
+    updaterange = unique(min(max(peakI-range:peakI+range,1),512));
+    testXHist(updaterange) = testXHist(updaterange) - gauss(updaterange);
+    
+    plot(bins,gauss);
+    plot(bins,testXHist);
+    
+    [peakV peakI] = max(testXHist);
+end
+
+length(peaksX)
+
+
+
+%%Y histogram
+[peakV peakI] = max(testYHist);
+sigma = 15;
+peaksY = [];
+
+figure
+plot(bins,testYHist);
+hold all
+while peakV > 500
+    peaksY = [peaksY peakI];
+    gauss = peakV * exp( - (bins-peakI).*(bins-peakI) / (2*sigma*sigma));
+    testYHist = testYHist - gauss;
+    
+    plot(bins,gauss);
+    plot(bins,testYHist);
+    
+    [peakV peakI] = max(testYHist);
+end
+
+length(peaksY)
+
+
+
+%%Z histogram
+[peakV peakI] = max(testZHist);
+sigma = 20;
+peaksZ = [];
+
+figure
+plot(bins,testZHist);
+hold all
+while peakV > 500
+    peaksZ = [peaksZ peakI];
+    gauss = peakV * exp( - (bins-peakI).*(bins-peakI) / (2*sigma*sigma));
+    testZHist = testZHist - gauss;
+    
+    plot(bins,gauss);
+    plot(bins,testZHist);
+    
+    [peakV peakI] = max(testZHist);
+end
+
+length(peaksZ)
