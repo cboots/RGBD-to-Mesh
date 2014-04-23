0cf30dee6cfd75901005a8696040d7e2bfc24924
Collin Boots <cboots@seas.upenn.edu>
2014-04-23 15:03:32 -0400

    Added visualization for quadtree buffer

---
+++ CUDA-Mesh/shaders/quadtreeFS.glsl
@@ -0,0 +1,28 @@
+#version 330
+
+uniform mat4 u_projMatrix;
+uniform mat4 u_viewMatrix;
+uniform sampler2D u_Texture0;
+uniform float u_TextureScale;
+
+in vec2 fs_texCoord;
+
+out vec4 FragColor;
+
+void main()
+{
+	//Just pass through for now
+	float quadtreeVal = texture(u_Texture0, fs_texCoord*u_TextureScale).x;
+	
+	if(isnan(quadtreeVal))
+	{
+		FragColor = 0.1*vec4(1.0);
+	}else{
+		int quadTreeDegree = int(quadtreeVal);
+		if(quadTreeDegree < 0)
+		{
+			FragColor = vec4(0.0);
+		}
+		FragColor = vec4((quadTreeDegree >> 6) & 0x7,(quadTreeDegree >> 3) & 0x7,quadTreeDegree & 0x7,1.0);
+	}
+}
